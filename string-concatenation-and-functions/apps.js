a0917d5059c11a0251f706ff46de12b29cac63eb
riya-varghese <57456903+riya-varghese@users.noreply.github.com>
2020-09-23 21:51:24 -0400

    haha just kidding the last one was a fake :)

---
+++ string-concatenation-and-functions/apps.js
@@ -0,0 +1,71 @@
+/*
+const myAge = 17;
+const yourAge = 38;
+console.log(myAge + yourAge);
+
+console.log("Hello my name is " + "Riya");// make sure to add a space after first one or the it will be isriya
+console.log('Hello it\'s me Riya')//if you use single quotes but one of ur words has apostrophe, you have to put a backslash before the apostrophe or replace with double quotes
+
+const name = "Riya";
+console.log(`Hey again it's me and my name is ${name}`)// that backtik thingy is above tab, $() is easier way to write the + ()
+
+const name = "Riya";
+const age = 17;
+console.log("Hi it's me " + name + " and I'm " + age + " years old. ");
+console.log(`Hello it's me ${name} and my age is ${age}`);// better way to write it
+
+const combined = name + age;
+console.log(combined);// since they're not both numbers and not both strings it becomes Riya17
+// to find data type u just do console.log(tyoeof combined);
+
+
+// FUNCTIONs
+
+
+const name = "Riya V";
+function logger() {
+    console.log(`Party Time');//shift+alt+down= copy paste down
+    console.log(`Party Time');
+    console.log(`Party Time');
+    console.log(`Party Time');
+}//only saves it to memory
+logger();//says it
+//whatever variables you add in the function aren't available outside the function
+// although whatever you put outside the function is available in the function
+
+const name = "Riya V";
+function adder(num1, num2) {
+    console.log(num1 + num2);
+}
+adder(5, 10);
+//way#1
+const name = "Riya V";
+const prisoner = `School`;
+function toUpper(text) {//made a parameter
+   const UpperCased = text.toUpperCase();
+   console.log(upperCased);
+}
+toUpper(name);
+toUpper(prisoner)
+//way#2
+const toUpper = function(){
+    heyooo
+}
+//way#3
+const toUpper = () => {
+    heyooo
+}
+
+
+//Number the math object
+
+
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with numbers
+val = num1 + num2;
+
+console.log(val);
+*/
