22288bd61b5ecef170221fe10e4803eca3b0af51
riya-varghese <57456903+riya-varghese@users.noreply.github.com>
2020-10-04 22:02:05 -0400

    switches and transversies

--- apps.js
+++ apps.js
@@ -41,4 +41,103 @@ user.sayName();
 
 //console.log(this);//shows everythinggg that we used thus for
 
+
+// switches
+const color = `yellow`;
+
+switch(color){
+    case `red`:
+        console.log(`Color is red`);
+        break;
+    case`blue`:
+        console.log(`Color is blue`);
+        break;
+    default:
+        console.log(`Color is not red or blue`);
+        break;
+}
+let day;
+
+switch(new Date().getDay()){
+    case 0;
+        day = `Sunday`;
+        break;
+    case 1;
+        day = `Monday`;
+        break;
+    case 2;
+        day = `Tuesday`;
+        break;
+    case 3;
+        day = `Wednesday`;
+        break;
+    case 4;
+        day = `Thursday`;
+        break;
+    case 5;
+        day = `Fridayday`;
+        break;
+    case 6;
+        day = `Saturday`;
+        break;
+}
+
+console.log(`Today is $(day)`);
+
+
+//Traversy Arrays
+//create some arrays
+const numbers = [43,56,33,23,44,36,5];
+const numbers2  = new Array(22,45,33,76,54);
+const fruit = [`apple`, `banana`, `orange`, `pear`];
+const mixed = [22, `Hello`, true, undefined, null, {a:1, b:1}, new Date()];
+
+//console.log(mixed)
+let val;
+
+// get array length
+val = numbers.length;
+//check if is array
+val = Array.isArray(numbers);
+//get single value
+val = numbers[3];//4th value because arrays are 0 based
+val = numbers[0];
+//insert into array
+numbers[2] = 100;
+//find index of value
+val = numbers.indexOf(36);
+
+//mutating arrays
+//add on to end
+numbers.push(250);
+//add onto front
+numbers.unshift(120);
+//take off from end
+numbers.pop();//took off 250
+//take off from front
+numbers.shift();
+//splice values
+numbers.splice(1,3);
+//reverse reverse
+numbers.reverse();
+
+//Concatonate arrays
+val = numbers.concat(numbers2);
+
+//sort
+val = fruit.sort();
+val = numbers.sort();//sorting by first number
+//use the "compare function"
+val = numbers.sort(function(x, y){
+    return x - y;//reverse sort change to y - x
+});
+
+//find
+function under50(num){
+    return num < 50;
+}
+val = numbers.find(under50);// can change to over and make < a >
+
+console.log(numbers);
+console.log(val);
 */
